230f215799b581abd345a9b810a45c5e6f02df37
jewblaster <149011784+n6ns@users.noreply.github.com>
2025-01-10 03:40:52 -0800

    Update draw.lua

--- draw.lua
+++ draw.lua
@@ -1,40 +1,42 @@
-	
+local draw = {}	
 
-local drawing_settings = {
-		Line = {
-			Thickness = 1;
-			Color = Color3fromRGB(255, 255, 255);
-		},
-		Text = {
-			Size = 13;
-			Center = true;
-			OutlineColor = Color3fromRGB(0, 0, 0);
-			Outline = true;
-			Font = 1;
-			Color = Color3fromRGB(255, 255, 255);
-		},
-		Square = {
-			Thickness = 1,
-			Color = Color3fromRGB(255, 255, 255);
-			Filled = false;
-		},
-		Triangle = {
-			Color = Color3fromRGB(255, 255, 255);
-			Filled = true;
-			Visible = false;
-			Thickness = 1;
-		}
+draw.settings = {
+	Line = {
+		Thickness = 1;
+		Color = Color3fromRGB(255, 255, 255);
+	},
+	Text = {
+		Size = 13;
+		Center = true;
+		OutlineColor = Color3fromRGB(0, 0, 0);
+		Outline = true;
+		Font = 1;
+		Color = Color3fromRGB(255, 255, 255);
+	},
+	Square = {
+		Thickness = 1,
+		Color = Color3fromRGB(255, 255, 255);
+		Filled = false;
+	},
+	Triangle = {
+		Color = Color3fromRGB(255, 255, 255);
+		Filled = true;
+		Visible = false;
+		Thickness = 1;
 	}
+}
 
-	local function draw(type)
-		local drawing = Drawing.new(type)
-
-		for i, v in drawing_settings[type] do
+function draw.new(type)
+	local drawing = Drawing.new(type)
+	
+	if draw.settings[type] then
+		for i, v in draw.settings[type] do
 			drawing[i] = v
 		end
-
-		return drawing
 	end
+	
+	return drawing
+end
 
 
 return draw
